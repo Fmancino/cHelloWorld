6e6d4ee83a9193b9cf0e3e60e5a5f61c44d8531c
Fmancino <17271080+Fmancino@users.noreply.github.com>
2020-01-14 18:09:09 +0100

    add common libraries and print of int

--- main.c
+++ main.c
@@ -1,10 +1,18 @@
 #include<stdio.h>
+// #include <stdlib.h> // standard library
+// #include <stdint.h> // uint32_t etc, defined in c99
 
 int main(int argc, char *argv[])
 {
     char *p;
+    int n;
+
     p="hello";
-    printf("%s you snooking\n", p);
+    printf("a string: %s \n", p);
+
+    n = 4;
+    printf("a number: %d \n",n);
+
     return 0;
 }
 
